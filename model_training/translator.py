cdc220221f78822783ae480ad54de1828418a373
TimurchikOgyrchik <134474047+TimurchikOgyrchik@users.noreply.github.com>
2025-02-22 02:30:54 +0900

    add translator to russian language

---
+++ model_training/translator.py
@@ -0,0 +1,38 @@
+import asyncio
+from googletrans import Translator
+
+emotion_dict = {
+    "love": "любовь",
+    "sadness": "печаль",
+    "joy": "радость",
+    "fear": "страх",
+    "anger": "гнев",
+    "surprise": "удивление"
+}
+
+
+async def translate_text(text, translator):
+    translation = await translator.translate(text, src='en', dest='ru')
+    return translation.text
+
+
+def translate_emotion(emotion, emotion_dict):
+    return emotion_dict.get(emotion, emotion)  # Если эмоция не найдена, возвращаем оригинал
+
+
+async def main():
+    translator = Translator()
+
+    with open('eu_train.txt', 'r', encoding='utf-8') as infile, open('ru_train.txt', 'w', encoding='utf-8') as outfile:
+        for line in infile:
+            text, emotion = line.strip().split(';')
+
+            translated_text = await translate_text(text, translator)
+            translated_emotion = translate_emotion(emotion, emotion_dict)
+
+            outfile.write(f"{translated_text};{translated_emotion}\n")
+    print('Все гуд!')
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
